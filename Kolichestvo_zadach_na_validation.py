9e89cf1783e211d2a160c6b2de801f60ab1e4e98
iPers <steacherok94@gmail.com>
2022-11-21 14:05:27 +0300

    Changes: - В print добавлено описание, что мы выводим

--- Kolichestvo_zadach_na_validation.py
+++ Kolichestvo_zadach_na_validation.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 response = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type':'VALIDATION'}).json()
-print(len(response['all']))
+print('На данный момент на валидации находится ', len(response['all']), ' задач')
