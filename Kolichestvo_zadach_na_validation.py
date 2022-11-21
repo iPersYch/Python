616182b2daa542660a9b42ddbadb43cd2f0d854e
iPers <steacherok94@gmail.com>
2022-11-21 16:22:46 +0300

    Changes: - Добавлен JSONDecoderError

--- Kolichestvo_zadach_na_validation.py
+++ Kolichestvo_zadach_na_validation.py
@@ -1,7 +1,11 @@
 import requests
+from json.decoder import JSONDecodeError
 
-response_valid = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type': 'VALIDATION'}).json()
-print('На данный момент на валидации находятся ', len(response_valid['all']), ' задач')
+try:
+    response_valid = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type': 'VALIDATION'}).json()
+    print('На данный момент на валидации находятся ', len(response_valid['all']), ' задач')
 
-response_dmca = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type':'DMCA'}).json()
-print('На данный момент на DMCA находятся ', len(response_dmca['all']), ' задач')
+    response_dmca = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type':'DMCA'}).json()
+    print('На данный момент на DMCA находятся ', len(response_dmca['all']), ' задач')
+except JSONDecodeError:
+    print('Respone not in .json')
