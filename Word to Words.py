161cd84c2e7381e61abd9e6c13eee313653baa97
iPers <steacherok94@gmail.com>
2022-11-21 18:10:13 +0300

    - Добавлен еще один вариант, генератора слов. - Сделан рефаторинг кода - Увеличена скорость работы алгоритма, но все еще недостаточно

--- Word to Words.py
+++ Word to Words.py
@@ -2,15 +2,18 @@
 import itertools
 import random
 import math
+import time
 
-word = 'сонгри'  #
+word = 'эскал'
 word_length = 3
 
 
 def word_to_words_v1(a=word, b=word_length):
+    start =time.time()
     comb = itertools.permutations(a, b)
     array = [''.join(i) for i in comb]
-    return print(sorted(array), '-----------  Способ через итертул')
+    end = time.time()-start
+    return print(sorted(array), '-----------  Способ через итертул, время выполнения= ',end)
 
 
 # Random ragemode will be there:)
@@ -32,6 +35,7 @@ def word_to_words_v2(a=word, b=word_length):  # Функция создает с
 
 
 def word_in_list(a=word, b=word_length):  # Функция вызывает предыдующую, которое создает слово, и проверяет есть ли оно в моем списке
+    start = time.time()
     while len(set(array_with_newWords_notSplited)) < math.factorial(len(a)) / math.factorial(
             len(a) - b):
         clean = word_to_words_v2(a, b)  # Вызываю функцию, которая генерирует слово
@@ -39,10 +43,32 @@ def word_in_list(a=word, b=word_length):  # Функция вызывает пр
             continue
         else:
             array_with_newWords_notSplited.append(word_to_words_v2(a, b))
-    return print(sorted(list(set(array_with_newWords_notSplited))), '-----------  Способ через рандом')
+    end = time.time()-start
+    return print(sorted(list(set(array_with_newWords_notSplited))), '-----------  Способ через рандом, время выполнения=', end)
 
 
-word_to_words_v1()  # Хоть способ через не особо продуктивный, но закончить его было делом принципа. Тут костыль в костыле просто :) И на больших числах он думает ооооочень долго.
-word_in_list()
+# Так же способ расчитанный на рандом, но сделанный через срезы и шафл
+def shuffle_words(a=word,b=word_length):
+    charlst = list(a)
+    random.shuffle(charlst)
+    new_word = ''.join(charlst)
+    return new_word[:b]
+def shuffled_arr(a=word,b=word_length):
+    start=time.time()
+    shuf_list=[]
+    while len(shuf_list) < math.factorial(len(a)) / math.factorial(len(a) - b):
+        shuffle_word = shuffle_words()
+        if shuffle_word not in shuf_list:
+            shuf_list.append(shuffle_word)
+        else:
+            continue
+    end=time.time()-start
+    return print(sorted(shuf_list), '-----------  Способ через shufl, время выполнения= ',end)
 
+word_to_words_v1()
+word_in_list() # Хоть способ через не особо продуктивный, но закончить его было делом принципа. Тут костыль в костыле просто :) И на больших числах он думает ооооочень долго.
+shuffled_arr() # Способ похож, на предыдущий, но выполнен иначе, благодаря чему скорость выполнения выше в несколько раз
 
+# На данном этапе я пришел к выводу, что с длинными словами будет работать только итертул.
+# Хоть мне и удалось оптимизировать код, через шафл и он начал работать быстрее этого недостаточно.
+# Необходимо придумать алгоритм, который исключит лишнии комбинации, которые появляются при рандоме и шафле.
