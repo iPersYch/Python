bcc5fc4a68c1c69a02e0fde0bd13a5f53585d9ad
iPers <steacherok94@gmail.com>
2022-11-21 17:01:00 +0300

    Changes: - Refactoring changes

--- Word to Words.py
+++ Word to Words.py
@@ -7,7 +7,7 @@ word = 'сонгри'  #
 word_length = 3
 
 
-def word_to_words_v1(a, b):
+def word_to_words_v1(a=word, b=word_length):
     comb = itertools.permutations(a, b)
     array = [''.join(i) for i in comb]
     return print(sorted(array), '-----------  Способ через итертул')
@@ -17,13 +17,13 @@ def word_to_words_v1(a, b):
 array_with_newWords_notSplited = []
 
 
-def word_to_words_v2(a,b):  # Функция создает слово по заданным параметрам
+def word_to_words_v2(a=word, b=word_length):  # Функция создает слово по заданным параметрам
     str_array = ''
     empty_array = []
     array_with_newwords = []
-    for i in word:
+    for i in a:
         empty_array.append(i)
-    while len(empty_array) > (len(word) - word_length):
+    while len(empty_array) > (len(a) - b):
         random_digit = random.randint(0, len(empty_array) - 1)
         array_with_newwords.append(empty_array[random_digit])
         empty_array.pop(random_digit)
@@ -31,19 +31,18 @@ def word_to_words_v2(a,b):  # Функция создает слово по за
     return str_array
 
 
-def asda(a, b):  # Функция вызывает предыдующую, которое создает слово, и проверяет есть ли оно в моем списке
-    while len(set(array_with_newWords_notSplited)) < math.factorial(len(word)) / math.factorial(
-            len(word) - word_length):
-        clean = word_to_words_v2(word, word_length)  # Вызываю функцию, которая генерирует слово
+def word_in_list(a=word, b=word_length):  # Функция вызывает предыдующую, которое создает слово, и проверяет есть ли оно в моем списке
+    while len(set(array_with_newWords_notSplited)) < math.factorial(len(a)) / math.factorial(
+            len(a) - b):
+        clean = word_to_words_v2(a, b)  # Вызываю функцию, которая генерирует слово
         if clean in array_with_newWords_notSplited:  # Проверяю есть ли ужеслово в списке. Время 4 часа утра, почему то проверка на вхождение срабатывала, но все равно появлялись дубликаты, пришлось делать множество, чтобы избавиться от них
             continue
         else:
-            array_with_newWords_notSplited.append(word_to_words_v2(word, word_length))
+            array_with_newWords_notSplited.append(word_to_words_v2(a, b))
     return print(sorted(list(set(array_with_newWords_notSplited))), '-----------  Способ через рандом')
 
 
-word_to_words_v1(word,
-                 word_length)  # Хоть способ через не особо продуктивный, но закончить его было делом принципа. Тут костыль в костыле просто :) И на больших числах он думает ооооочень долго.
-asda(word, word_length)
+word_to_words_v1()  # Хоть способ через не особо продуктивный, но закончить его было делом принципа. Тут костыль в костыле просто :) И на больших числах он думает ооооочень долго.
+word_in_list()
 
 
