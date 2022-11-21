aea1a5c28c8cb0cf7bdefd38cabd7ed07b7c1efa
iPers <steacherok94@gmail.com>
2022-11-21 15:59:04 +0300

    Changes: - Добавлена функция подсчета задач по dmca

--- Kolichestvo_zadach_na_validation.py
+++ Kolichestvo_zadach_na_validation.py
@@ -1,4 +1,7 @@
 import requests
 
-response = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type':'VALIDATION'}).json()
-print('На данный момент на валидации находится ', len(response['all']), ' задач')
+response_valid = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type': 'VALIDATION'}).json()
+print('На данный момент на валидации находятся ', len(response_valid['all']), ' задач')
+
+response_dmca = requests.get('https://providence.kazan.startru.tech/api/v1/tasks_tracker', params={'task_type':'DMCA'}).json()
+print('На данный момент на DMCA находятся ', len(response_dmca['all']), ' задач')
